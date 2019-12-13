b29cee4c7f077b967b4f49979a1b9dd9f86e59e6
Nam Nguyen <phuongnam@phuongnam.org>
2019-12-13 16:16:57 +0700

    fix

---
+++ vst-install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bash vst-install.sh --nginx yes --phpfpm yes --apache no --named yes --remi yes --vsftpd yes --proftpd no --iptables no --fail2ban no --quota no --exim yes --dovecot yes --spamassassin no --clamav no --softaculous no --mysql yes --postgresql no --email phuongnam@phuongnam.org
